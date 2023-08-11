dac39c25400da69a706705d3e91fd1faa1bc0476
matthewt-assurity <matthew.tansley@assurity.co.nz>
2023-08-12 11:52:20 +1200

    Extract the content of a message from an API response

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import openai
 import os
+import json
 
 url = "https://community.notepad-plus-plus.org/topic/24802/notepad-v8-5-5-release"
 
@@ -11,7 +12,8 @@ rolePrompt = open("role.txt","r").read()
 message = {"role": "user", "content": rolePrompt}
 roleResponse = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[message])
 
-print(chat_completion)
+# roleResponseJson = json.loads(roleResponse.toString())
+print(roleResponse['choices'][0]['message']['content'])
 
 # Send an HTTP GET request
 # response = requests.get(url)
@@ -36,17 +38,19 @@ print(chat_completion)
 #     print("Failed to retrieve the web page.")
 
 
-prompt = "Create a test case based on this release note. Relase note 1: Update to Scintilla 5.3.6 and Lexilla 5.2.6 (Implement #13940, fix #4741,  #13901, #13943, #13911)"
+# prompt = "Create a test case based on this release note. Relase note 1: Update to Scintilla 5.3.6 and Lexilla 5.2.6 (Implement #13940, fix #4741,  #13901, #13943, #13911)"
 
-rolePromptMessage = {"role": "user", "content": rolePrompt}
-roleResponseMessage = {"role": "assistant", "content",roleResponse}
-message = {"role": "user", "content": prompt}
+# rolePromptMessage = {"role": "user", "content": rolePrompt}
+# roleResponseMessage = {"role": "assistant", "content": roleResponse.choices[0].content}
+# message = {"role": "user", "content": prompt}
 
-chat_completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo", 
-    messages=[
-        
-    ],
-)
+# chat_completion = openai.ChatCompletion.create(
+#     model="gpt-3.5-turbo", 
+#     messages=[
+#         rolePromptMessage,
+#         roleResponseMessage,
+#         message
+#     ],
+# )
 
-print(chat_completion)
+# print(chat_completion)
