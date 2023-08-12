a541c3281e065f55dca98c768551549d807b271d
matthewt-assurity <matthew.tansley@assurity.co.nz>
2023-08-12 12:01:17 +1200

    Output message content only not whole json

--- chatgpt.py
+++ chatgpt.py
@@ -3,10 +3,10 @@ import os
 
 openai.api_key = open("openai_key.txt","r").read()
 
-prompt = "Hello ChatGPT"
-# prompt = "Create a test scenario for this release note: Make tab context menu less cluttered by adding sub-menu."
+# prompt = "Hello ChatGPT"
+prompt = "Create a test scenario for this release note: Make tab context menu less cluttered by adding sub-menu."
 message = {"role": "user", "content": prompt}
 
 chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[message])
 
-print(chat_completion)
+print(chat_completion['choices'][0]['message']['content'])
